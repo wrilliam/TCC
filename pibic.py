4d3009a41cbad8a3915f05495b90d36462e8fc38
Ribeiro <7347861+wrilliam@users.noreply.github.com>
2018-10-02 21:59:06 -0300

    Powering up

--- pibic.py
+++ pibic.py
@@ -16,13 +16,7 @@ y, sr = librosa.load(filename)
 # 3. Run the default beat tracker
 tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
 
-print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
-
-if tempo >= ZERO:
-	mood = 'excited'
-else:
-	mood = 'calm'
-print('Mood:',mood)
+# print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
 
 # 4. Convert the frame indices of beat events into timestamps
 # beat_times = librosa.frames_to_time(beat_frames, sr=sr)
@@ -30,5 +24,20 @@ print('Mood:',mood)
 # print('Saving output to beat_times.csv')
 # librosa.output.times_csv('beat_times.csv', beat_times)
 
+# First metric: duration
+duration = librosa.get_duration(filename)
+print('Duration (in seconds): ', duration)
+
+# Second metric: mood
+if tempo >= ZERO: mood = 'excited'
+else: mood = 'calm'
+print('Mood: ', mood)
+
 y_harmonic, y_percussive = librosa.effects.hpss(y, margin=(1.0,5.0))
 librosa.output.write_wav('testing.wav', y_percussive, sr)
+
+# Third metric: max power (in dBFS)
+S = np.abs(librosa.stft(y))
+array = librosa.power_to_db(S**2)
+power = array.max()
+print('Max power (in dBFS): ', power)
