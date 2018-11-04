b6c1ea30623cfa9518f30a6a3043566266d692ab
Ribeiro <7347861+wrilliam@users.noreply.github.com>
2018-11-04 17:32:05 -0300

    Some last tweaks

--- pibic.py
+++ pibic.py
@@ -10,7 +10,8 @@ import librosa
 # filename = 'C:\\TCC-master\\kalimba.mp3'
 # filename = 'C:\\TCC-master\\harp.mp3'
 # filename = 'C:\\TCC-master\\topgear.mp3'
-filename = 'C:\\TCC-master\\hadou.mp3'
+# filename = 'C:\\TCC-master\\hadou.mp3'
+filename = 'C:\\TCC-master\\music\\Woodlands.mp3'
 
 # 1b. Get all audio files in a directory sub-tree
 # files = librosa.util.find_files('~/Music')
@@ -27,8 +28,8 @@ y, sr = librosa.load(filename)
 tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
 
 # 3b. Get the tempo
-onset_env = librosa.onset.onset_strength(y, sr=sr)
-tempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr)
+# onset_env = librosa.onset.onset_strength(y, sr=sr)
+# tempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr)
 
 # 4. Convert the frame indices of beat events into timestamps
 # beat_times = librosa.frames_to_time(beat_frames, sr=sr)
@@ -38,23 +39,23 @@ tempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr)
 
 # First metric: duration
 duration = librosa.get_duration(y)
-print('Duration (in seconds): ', duration)
+print('Duration: {:.2f} seconds'.format(duration))
 
 # Second metric: pace
-if tempo < 52: pace = 'broadly slow'
-  else
-  if tempo < 76: pace = 'slow'
-  else
-  if tempo < 100: pace = 'moderately slow'
-  else
-  if tempo < 112: pace = 'somewhat fast'
-  else
-  if tempo < 138: pace = 'fast'
-  else
-  if tempo < 143: pace = 'fast & lively'
-  else
-  if tempo < 188: pace = 'very fast'
-  else: pace = 'broadly fast'
+if tempo < 52.0: pace = 'broadly slow'
+else:
+    if tempo < 76.0: pace = 'slow'
+    else:
+        if tempo < 100.0: pace = 'moderately slow'
+        else:
+            if tempo < 112.0: pace = 'somewhat fast'
+            else:
+                if tempo < 138.0: pace = 'fast'
+                else:
+                    if tempo < 143.0: pace = 'fast & lively'
+                    else:
+                        if tempo < 188.0: pace = 'very fast'
+                        else: pace = 'broadly fast'
 print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
 print('Pace: ', pace)
 
@@ -67,10 +68,16 @@ S = np.abs(librosa.stft(y_percussive))
 array = librosa.power_to_db(S**2)
 maxpower = array.max()
 minpower = array.min()
-print(array)
 print('Max power (in dB): ', maxpower)
-print('Min power (in dB): ', minpower)
 
 if maxpower >= 30.0: percussion = 'featured'
 else: percussion = 'absent'
 print('Percussion: ', percussion)
+
+# Testing queries
+if pace == 'slow' and duration <= 180: print('Query 1: true')
+else: print('Query 1: false')
+if pace == 'fast' and percussion == 'featured': print('Query 2: true')
+else: print('Query 2: false')
+if percussion == 'absent' and duration >= 120: print('Query 3: true')
+else: print('Query 3: false')
