ddabceb06a30cdad29fc003a228c6e4f38b48559
Ribeiro <7347861+wrilliam@users.noreply.github.com>
2018-11-04 04:03:32 -0300

    Keeping up the pace
    Okay, bad joke...

--- pibic.py
+++ pibic.py
@@ -3,8 +3,6 @@ import numpy as np
 import soundfile as sf
 import librosa
 
-PACE = 110.0
-
 # 1. Get the file path to the included audio example
 # filename = librosa.util.example_audio_file()
 
@@ -12,10 +10,10 @@ PACE = 110.0
 # filename = 'C:\\TCC-master\\kalimba.mp3'
 # filename = 'C:\\TCC-master\\harp.mp3'
 # filename = 'C:\\TCC-master\\topgear.mp3'
-# filename = 'C:\\TCC-master\\hadou.mp3'
+filename = 'C:\\TCC-master\\hadou.mp3'
 
 # 1b. Get all audio files in a directory sub-tree
-files = librosa.util.find_files('~/Music')
+# files = librosa.util.find_files('~/Music')
 
 # 1c. Read from an URL
 # url = "https://raw.githubusercontent.com/librosa/librosa/master/tests/data/test1_44100.wav"
@@ -24,7 +22,6 @@ files = librosa.util.find_files('~/Music')
 # 2. Load the audio as a waveform `y`, and store the sampling rate as `sr`
 y, sr = librosa.load(filename)
 # y, sr = sf.read(filename)
-# y, sr = librosa.load(files)
 
 # 3a. Run the default beat tracker
 tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
@@ -44,44 +41,20 @@ duration = librosa.get_duration(y)
 print('Duration (in seconds): ', duration)
 
 # Second metric: pace
-if tempo < 40: pace = 'larghissimo'
+if tempo < 52: pace = 'broadly slow'
   else
-  if tempo < 52: pace = 'largo'
+  if tempo < 76: pace = 'slow'
   else
-  if tempo < 60: pace = 'largo-lento'
+  if tempo < 100: pace = 'moderately slow'
   else
-  if tempo < 61: pace = 'largo-lento-adagio'
+  if tempo < 112: pace = 'somewhat fast'
   else
-  if tempo < 69: pace = 'lento-adagio'
+  if tempo < 138: pace = 'fast'
   else
-  if tempo < 76: pace = 'adagio'
+  if tempo < 143: pace = 'fast & lively'
   else
-  if tempo < 81: pace = 'adagio-andante'
-  else
-  if tempo < 88: pace = 'andante'
-  else
-  if tempo < 100: pace = 'andante-moderato'
-  else
-  if tempo < 101: pace = 'andante-moderato-allegretto'
-  else
-  if tempo < 112: pace = 'moderato-allegretto'
-  else
-  if tempo < 113: pace = 'moderato-allegretto-allegro'
-  else
-  if tempo < 129: pace = 'allegretto-allegro'
-  else
-  if tempo < 138: pace = 'allegro'
-  else
-  if tempo < 140: pace = 'allegro-vivace'
-  else
-  if tempo < 143: pace = 'allegro-vivace-presto'
-  else
-  if tempo < 161: pace = 'allegro-presto'
-  else
-  if tempo < 188: pace = 'presto'
-  else
-  if tempo < 201: pace = 'presto-prestissimo'
-  else: pace = 'prestissimo'
+  if tempo < 188: pace = 'very fast'
+  else: pace = 'broadly fast'
 print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
 print('Pace: ', pace)
 
